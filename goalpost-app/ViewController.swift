c18f223a65695ad779498fc7651c383ee16ecfcb
RuliSlim <ruli0to8@gmail.com>
2020-07-03 14:51:01 +0700

    Initial Commit

---
+++ goalpost-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Ruli on 03/07/20.
+//  Copyright © 2020 Ruli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
